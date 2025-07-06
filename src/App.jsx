b9cb5620db6d83e2ba5d1476fd3505ef859b6a96
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-06 23:19:41 +0530

    Login / Sign Up Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import List from './components/list/List.jsx';
-import Chat from './components/chat/Chat.jsx';
-import Detail from './components/detail/detail.jsx';
+import React from "react";
+import List from "./components/list/List.jsx";
+import Chat from "./components/chat/Chat.jsx";
+import Detail from "./components/detail/detail.jsx";
+import Login from "./components/login/Login.jsx";
 
 const App = () => {
-  return(
+  const user = false;
+
+  return (
     <div className="container">
-      <List/>
-      <Chat/>
-      <Detail/>
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App;
