a56b5591ebc33673abff5e51968aeb48151dd1c9
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-08 06:02:39 +0530

    Add an Image to the Chat Message

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -13,11 +13,16 @@ import {
 import { db } from "../../lib/firebase";
 import { useChatStore } from "../../lib/chatStore";
 import { useUserStore } from "../../lib/userStore";
+import upload from "../../lib/upload";
 
 const Chat = () => {
   const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+  const [img, setImg] = useState({
+    file: null,
+    url: "",
+  });
 
   const { currentUser } = useUserStore();
   const { chatId, user } = useChatStore();
@@ -44,15 +49,31 @@ const Chat = () => {
     setOpen(false);
   };
 
+  const handleImg = (e) => {
+    if (e.target.files[0]) {
+      setImg({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+
   const handleSend = async () => {
     if (text === "") return;
 
+    let imgUrl = null;
+
     try {
+      if (img.file) {
+        imgUrl = await upload(img.file);
+      }
+
       await updateDoc(doc(db, "chats", chatId), {
         messages: arrayUnion({
           senderId: currentUser.id,
           text,
           createdAt: new Date(),
+          ...(imgUrl && { img: imgUrl }),
         }),
       });
 
@@ -70,7 +91,8 @@ const Chat = () => {
           );
 
           userChatsData.chats[chatIndex].lastMessage = text;
-          userChatsData.chats[chatIndex].isSeen = id === currentUser.id ? true : false;
+          userChatsData.chats[chatIndex].isSeen =
+            id === currentUser.id ? true : false;
           userChatsData.chats[chatIndex].updatedAt = Date.now();
 
           await updateDoc(userChatsRef, {
@@ -81,6 +103,11 @@ const Chat = () => {
     } catch (err) {
       console.log(err);
     }
+    setImg({
+      file: null,
+      url: "",
+    });
+    setText("");
   };
 
   //   console.log(text);
@@ -111,7 +138,7 @@ const Chat = () => {
       >
         <div className="center">
           {chat?.messages?.map((message) => (
-            <div className="message own" key={message?.createAt}>
+            <div className={message.senderId === currentUser?.id ? "message own" : "message"} key={message?.createAt}>
               <div className="texts">
                 {message.img && <img src={message.img} alt="" />}
                 <p>{message.text}</p>
@@ -119,12 +146,27 @@ const Chat = () => {
               </div>
             </div>
           ))}
+          {img.url && (
+            <div className="message own">
+              <div className="texts">
+                <img src={img.url} alt="" />
+              </div>
+            </div>
+          )}
           <div ref={endRef}></div>
         </div>
       </PerfectScrollbar>
       <div className="bottom">
         <div className="icons">
-          <img src="./img.png" alt="" />
+          <label htmlFor="file">
+            <img src="./img.png" alt="" />
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleImg}
+          />
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
