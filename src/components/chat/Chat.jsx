0a154c812d03c63086f9f93387d446ef93c5dc8a
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-08 05:41:56 +0530

    Adding a New Chat Message

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,39 +1,86 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import EmojiPicker from "emoji-picker-react";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "./Chat.css";
-import { doc, onSnapshot } from "firebase/firestore";
+import {
+  arrayUnion,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../../lib/firebase";
 import { useChatStore } from "../../lib/chatStore";
+import { useUserStore } from "../../lib/userStore";
 
 const Chat = () => {
   const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
 
-  const { chatId } = useChatStore();
+  const { currentUser } = useUserStore();
+  const { chatId, user } = useChatStore();
 
   const endRef = useRef(null);
 
-  useEffect(()=>{
-    endRef.current?.scrollIntoView({behavior:"smooth"})
-  },[])
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
-  useEffect(()=>{
-    const unSub = onSnapshot(doc(db,"chats", chatId), (res) => {
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", chatId), (res) => {
       setChat(res.data());
-    })
+    });
     return () => {
       unSub();
-    }
-  },[chatId])
+    };
+  }, [chatId]);
 
-  console.log(chat);
+  // console.log(chat);
 
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
+  };
+
+  const handleSend = async () => {
+    if (text === "") return;
+
+    try {
+      await updateDoc(doc(db, "chats", chatId), {
+        messages: arrayUnion({
+          senderId: currentUser.id,
+          text,
+          createdAt: new Date(),
+        }),
+      });
+
+      const userIDs = [currentUser.id, user.id];
+
+      userIDs.forEach(async (id) => {
+        const userChatsRef = doc(db, "userchats", id);
+        const userChatsSnapshot = await getDoc(userChatsRef);
+
+        if (userChatsSnapshot.exists()) {
+          const userChatsData = userChatsSnapshot.data();
+
+          const chatIndex = userChatsData.chats.findIndex(
+            (c) => c.chatId === chatId
+          );
+
+          userChatsData.chats[chatIndex].lastMessage = text;
+          userChatsData.chats[chatIndex].isSeen = id === currentUser.id ? true : false;
+          userChatsData.chats[chatIndex].updatedAt = Date.now();
+
+          await updateDoc(userChatsRef, {
+            chats: userChatsData.chats,
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   //   console.log(text);
@@ -63,73 +110,15 @@ const Chat = () => {
         }}
       >
         <div className="center">
-          <div className="message own">
-            <div className="texts">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhqT4UBhTEjhlwPcwK9H7HJqKLUY2sPMNyjw&s"
-                alt=""
-              />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quibusdam necessitatibus ex a quas? Enim error quod atque!
-                Animi, nisi officia.
-              </p>
-              <span>1 min ago</span>
+          {chat?.messages?.map((message) => (
+            <div className="message own" key={message?.createAt}>
+              <div className="texts">
+                {message.img && <img src={message.img} alt="" />}
+                <p>{message.text}</p>
+                <span>1 min ago</span>
+              </div>
             </div>
-          </div>
-          <div className="message">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quibusdam necessitatibus ex a quas? Enim error quod atque!
-                Animi, nisi officia.
-              </p>
-              <span>1 min ago</span>
-            </div>
-          </div>
-          <div className="message own">
-            <div className="texts">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quibusdam necessitatibus ex a quas? Enim error quod atque!
-                Animi, nisi officia.
-              </p>
-              <span>1 min ago</span>
-            </div>
-          </div>
-          <div className="message">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quibusdam necessitatibus ex a quas? Enim error quod atque!
-                Animi, nisi officia.
-              </p>
-              <span>1 min ago</span>
-            </div>
-          </div>
-          <div className="message own">
-            <div className="texts">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quibusdam necessitatibus ex a quas? Enim error quod atque!
-                Animi, nisi officia.
-              </p>
-              <span>1 min ago</span>
-            </div>
-          </div>
-          <div className="message">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quibusdam necessitatibus ex a quas? Enim error quod atque!
-                Animi, nisi officia.
-              </p>
-              <span>1 min ago</span>
-            </div>
-          </div>
+          ))}
           <div ref={endRef}></div>
         </div>
       </PerfectScrollbar>
@@ -155,7 +144,9 @@ const Chat = () => {
             <EmojiPicker open={open} onEmojiClick={handleEmoji} />
           </div>
         </div>
-        <button className="sendButton">Send</button>
+        <button className="sendButton" onClick={handleSend}>
+          Send
+        </button>
       </div>
     </div>
   );
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -4,7 +4,7 @@ import PerfectScrollbar from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "./ChatList.css";
 import { useUserStore } from "../../../lib/userStore";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
 import { useChatStore } from "../../../lib/chatStore";
 
@@ -46,8 +46,27 @@ const ChatList = () => {
   // console.log(chats);
 
   const handleSelect = async (chat) => {
-    changeChat(chat.chatId, chat.user);
-  }
+    
+    const userChats = chats.map(item=>{
+      const {user, ...rest} = item;
+      return rest;
+    })
+
+    const chatIndex = userChats.findIndex(item=>item.chatId === chat.chatId);
+
+    userChats[chatIndex].isSeen = true;
+
+    const userChatsRef = doc(db, "userchats", currentUser.id);
+
+    try{
+      await updateDoc(userChatsRef, {
+        chats: userChats,
+      });
+      changeChat(chat.chatId, chat.user);
+    } catch(err){
+      console.log(err);
+    }
+  };
 
   return (
     <div className="chatList">
@@ -72,7 +91,14 @@ const ChatList = () => {
         }}
       >
         {chats.map((chat) => (
-          <div className="item" key={chat.chatId} onClick={()=>handleSelect(chat)}>
+          <div
+            className="item"
+            key={chat.chatId}
+            onClick={() => handleSelect(chat)}
+            style={{
+              backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
+            }}
+          >
             <img src={chat.user.avatar || "./avatar.png"} alt="" />
             <div className="texts">
               <span>{chat.user.username}</span>
