ea614b82c005656e09463b12e363ffb76ba23016
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-10 03:16:02 +0530

    Ready For Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import List from "./components/list/List.jsx";
 import Chat from "./components/chat/Chat.jsx";
-import Detail from "./components/detail/detail.jsx";
+import Detail from "./components/detail/Detail.jsx";
 import Login from "./components/login/Login.jsx";
 import Notification from "./components/notification/Notification.jsx";
 import { onAuthStateChanged } from "firebase/auth";
